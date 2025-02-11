89fac296b2faf162af07dbf819492f71c62af2c7
Christian Batista <me@christianbatista.com>
2025-02-11 17:20:04 -0300

    script build v2

--- script/setup-deb-structure.sh
+++ script/setup-deb-structure.sh
@@ -9,8 +9,8 @@ mkdir -p $BASE_DIR/usr/share/icons/Kaze-light
 mkdir -p $BASE_DIR/usr/share/icons/Kaze-Dark
 
 # Movendo os ícones para os locais corretos
-cp -r $BASE_DIR/Kaze-light/* $BASE_DIR/usr/share/icons/Kaze-light/
-cp -r $BASE_DIR/Kaze-Dark/* $BASE_DIR/usr/share/icons/Kaze-Dark/
+cp -r $BASE_DIR/Kaze-light $BASE_DIR/usr/share/icons/Kaze-light
+cp -r $BASE_DIR/Kaze-Dark $BASE_DIR/usr/share/icons/Kaze-Dark
 
 # Removendo diretórios antigos
 rm -rf $BASE_DIR/Kaze-light
